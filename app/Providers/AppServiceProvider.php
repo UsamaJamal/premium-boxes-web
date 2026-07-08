175716f5a607a95a669068b40a58fa51ebacd64c
FarhanQayyum <mfaari83@gmail.com>
2026-07-08 16:05:51 +0500

    dfsdfsdf

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if(env('APP_ENV') !== 'local' || env('APP_ENV') === 'local') { // Force on live, but since their .env on Hostinger might be 'local', let's just force it if the scheme isn't specifically disabled. Let's just force it.
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
