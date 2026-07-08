f2948d1800c673549b1af0fd7e7ce1cb92435eb5
FarhanQayyum <mfaari83@gmail.com>
2026-07-08 16:22:04 +0500

    Fix syntax errors caused by trailing slash removal

--- resources/views/adminlte/dynamic/addpage.blade.php
+++ resources/views/adminlte/dynamic/addpage.blade.php
@@ -10,7 +10,7 @@
                 <!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="{{ url('admin/dashboard')" style="color: #234376;">Home</a></li>
+                        <li class="breadcrumb-item"><a href="{{ url('admin/dashboard') }}" style="color: #234376;">Home</a></li>
                         <li class="breadcrumb-item active" style="color: #49d8f7;">Add Page</li>
                     </ol>
                 </div>
