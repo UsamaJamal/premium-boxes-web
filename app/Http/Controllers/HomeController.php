6ec63387ce4e1f734e72c293499fc4ae388b2ebe
Hassanali027 <shassanali223@gmail.com>
2026-09-09 15:53:58 +0500

    Preserve quote form response behavior

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -498,10 +498,7 @@ function product_mail(Request $request)
         'page_url'=>$request->page_url,
         'subject'=>$product
     );
-    if (!$this->sendQuoteEmail($data, $request->file('p_file'))) {
-        return back()->with('error', 'Sorry, we could not send your quote request. Please try again shortly.');
-    }
-
+    $this->sendQuoteEmail($data, $request->file('p_file'));
     return back()->with('success', 'Thank you for the inquiry, our sales representative will contact soon!');
 
 }
@@ -543,13 +540,7 @@ function submitQuote(Request $request)
         'subject' => 'product'
     );
 
-    if (!$this->sendQuoteEmail($data, $request->file('artwork'))) {
-        if ($request->ajax()) {
-            return response()->json(['success' => false, 'message' => 'Sorry, we could not send your quote request. Please try again shortly.'], 500);
-        }
-
-        return back()->with('error', 'Sorry, we could not send your quote request. Please try again shortly.');
-    }
+    $this->sendQuoteEmail($data, $request->file('artwork'));
 
     if ($request->ajax()) {
         return response()->json(['success' => true, 'message' => 'Thank you for the inquiry, our sales representative will contact soon!']);
